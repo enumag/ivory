d1a762d84dfd72d9c2b57e4bbb60ebc76df5e54c
Jáchym Toušek <enumag@gmail.com>
2011-11-29 14:19:33 +0100

    Třída pro globální blok

---
+++ Global.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package Ivory
+ * @license MIT, LGPL, BSD
+ * @copyright (c) 2011 Jáchym Toušek
+ */
+
+namespace Ivory\StyleSheets;
+
+/**
+ * @author Jáchym Toušek
+ */
+class Global extends Block {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
