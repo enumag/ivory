e655a18c560e33d6936fcef34b54ee006bb1c7d0
Jáchym Toušek <enumag@gmail.com>
2011-12-15 09:25:34 +0100

    Přidán Media blok

---
+++ IvoryStyleSheets/Media.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package Ivory
+ * @license MIT, LGPL, BSD
+ * @copyright (c) 2011 Jáchym Toušek
+ */
+
+namespace Ivory\StyleSheets;
+
+/**
+ * @author Jáchym Toušek
+ */
+class Media extends AtRule {
+
+
+
+}
